f88b5257664dfee67493c2f98fa4be43c485f8de
AIex05 <alex.yt.zhu@gmail.com>
2023-01-18 20:55:12 -0500

    Finalized project_0 except README.md

--- project_0/test.c
+++ project_0/test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <string.h>
+#include  <string.h>
 #include <stdlib.h>
+
 int main () {
-char *buffer;
-strncpy(buffer, "Hello, world!", 14);
-printf("%s\n", buffer);
-exit(0);
+	char *buffer;
+	strncpy(buffer, "Hello, world!", 14);
+	printf("%s\n", buffer);
+	exit(0);
 }
